8a4bd23eb939502ecd3bbf8417f743e3e7cb36b7
swde78 <33987839+swde78@users.noreply.github.com>
2024-12-28 10:40:08 +0200

    Update New Person Test webhook

--- src/main/java/com/example/service/PersonService.java
+++ src/main/java/com/example/service/PersonService.java
@@ -39,7 +39,13 @@ public class PersonService {
 		p.setFirstName("AbdulHalim");
 		p.setLastName("Mahmoud");
 		persons.add(p);
-		
+
+		p = new Person();
+		p.setId("5");
+		p.setAge(35);
+		p.setFirstName("Mohamed");
+		p.setLastName("Swisi");
+		persons.add(p);
 	}
 	public Person getPerson(String id) {
 		for(Person person:persons) {
